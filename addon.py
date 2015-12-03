8ca0d7bfb861a64d821d93cff04f2e05a3a6fd6b
Vagios Vlachos <bagios234@gmail.com>
2015-12-03 23:47:55 +0200

    Change some text and name

--- addon.py
+++ addon.py
@@ -1,11 +1,11 @@
 ﻿import xbmcaddon
 import xbmcgui
- 
+
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
+
+line1 = "Goodbye World!"
+line2 = "And welcome back"
+line3 = "My own Add-On"
+
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
